cb2636448c2c839a0c75d6a2d86e2d875da37537
om2897 <omc2897@gmail.com>
2025-09-11 19:19:35 +0530

    Add files via upload

---
+++ accept10value and print 4,7,9 value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int a[10],i,j,t;
+    printf("enter 5 values");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+    }
+    printf("the 4th value is %d\n",a[3]);
+    printf("the 7th value is %d\n",a[6]);
+    printf("the 9th value is %d\n",a[8]);
+}
